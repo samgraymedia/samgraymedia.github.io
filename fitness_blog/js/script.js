888f7339e5461118a73943004d19aaa2d157d26a
Samuel Gray <sam.gray22@gmail.com>
2017-05-09 15:14:48 +0100

    stuff and stuff

--- fitness_blog/js/script.js
+++ fitness_blog/js/script.js
@@ -1,9 +1,20 @@
 var options = {
-    byRow: true,
-    property: 'min-height',
-    target: null,
-    remove: false
+  byRow: true,
+  property: 'min-height',
+  target: null,
+  remove: false
 };
 $(function() {
-	$('.pod').matchHeight(options);
+  $('.pod').matchHeight(options);
+});
+
+
+$('#popup-js').click(function(e) {
+  $('#top').addClass('filter');
+  $('.search-popup').css('display', 'block');
+  e.stopPropagation();
+});
+$("body").click(function() {
+  $('#top').removeClass('filter');
+  $('.search-popup').css('display', 'none');
 });
