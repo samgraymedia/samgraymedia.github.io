0c1513422df12aef1013f34f95da61e0923d9af7
Samuel Gray <sam.gray22@gmail.com>
2017-06-01 21:44:56 +0100

    base gulp done

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     pug = require('gulp-pug'),
-    autoprefixer = require('gulp-autoprefixer');
+    autoprefixer = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync').create();
 
 //SASS TASK
 gulp.task('styles', function() {
     gulp.src('my_portfolio/sass/**/*.sass')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('my_portfolio/sass/css'));
+        .pipe(gulp.dest('my_portfolio/sass/css'))
+        .pipe(browserSync.stream());
 });
 
 //PUG TASK
@@ -28,11 +30,24 @@ gulp.task('autoprefixer', () =>
 );
 
 
-// WATCH
-gulp.task('watch',function() {
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['styles'], function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
     gulp.watch('my_portfolio/sass/**/*.sass',['styles']);
-    gulp.watch('my_portfolio/sass/css/*.css',['autoprefixer']);
-    gulp.watch('my_portfolio/pug/**/*.pug',['markup']);
+    gulp.watch('my_portfolio/sass/css/*.css',['autoprefixer']).on('change', browserSync.reload);
+    gulp.watch('my_portfolio/pug/**/*.pug',['markup']).on('change', browserSync.reload);
 });
 
-gulp.task('default', ['styles', 'markup', 'autoprefixer', 'watch']);
+
+
+// WATCH
+gulp.task('watch',function() {
+
+});
+
+gulp.task('default', ['styles', 'markup', 'autoprefixer', 'watch', 'serve' ]);
