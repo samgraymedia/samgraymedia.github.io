a01669faad528a8b59cbf1447a7b2593e7e19fc3
Samuel Gray <sam.gray22@gmail.com>
2017-06-02 11:57:58 +0100

    set up 1.0.0

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,10 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     pug = require('gulp-pug'),
     autoprefixer = require('gulp-autoprefixer'),
+    clean = require('gulp-clean'),
+    minify = require('gulp-minify'),
     browserSync = require('browser-sync').create();
+
 
 //SASS TASK
 gulp.task('styles', function() {
@@ -30,7 +33,25 @@ gulp.task('autoprefixer', () =>
         .pipe(gulp.dest('my_portfolio/dest/css/'))
 );
 
+//CLEAN TASK
 
+gulp.task('clean', function(){
+  return gulp.src('my_portfolio/src/sass/css', {read: false})
+        .pipe(clean());
+});
+
+//JS TASK
+gulp.task('minify', function() {
+  gulp.src('my_portfolio/src/js/*.js')
+    .pipe(minify({
+        ext:{
+
+
+            min:'.js'
+        }
+    }))
+    .pipe(gulp.dest('my_portfolio/dest/js'))
+});
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['styles'], function() {
@@ -40,6 +61,7 @@ gulp.task('serve', ['styles'], function() {
     });
 
     gulp.watch('my_portfolio/src/sass/**/*.sass',['styles']);
+    gulp.watch('my_portfolio/src/js/*.js',['minify']).on('change', browserSync.reload);
     gulp.watch('my_portfolio/src/sass/css/*.css',['autoprefixer']).on('change', browserSync.reload);
     gulp.watch('my_portfolio/src/pug/**/*.pug',['markup']).on('change', browserSync.reload);
 });
@@ -51,4 +73,4 @@ gulp.task('watch',function() {
 
 });
 
-gulp.task('default', ['styles', 'markup', 'autoprefixer', 'watch', 'serve' ]);
+gulp.task('default', ['styles', 'markup', 'autoprefixer', 'clean', 'minify', 'watch', 'serve' ]);
